bc22d2692e013986d1199e482c2cfa01dd6e7aa8
Raphael Moraes <raphaelmoraes@MacBook-Air-de-Raphael.local>
2026-08-18 18:15:04 -0300

    Publish student finance center frontend

--- auth/config.js
+++ auth/config.js
@@ -1,6 +1,6 @@
 window.PurpleAuthConfig=Object.freeze({
-  appVersion:'1.10.14-turmas-fix',
-  serviceWorkerVersion:'purple-gestao-v156',
+  appVersion:'1.10.15-finance-central',
+  serviceWorkerVersion:'purple-gestao-v159',
   manifestVersion:'manifest.webmanifest',
   supabaseUrl:'https://qqlymzyvvgmbyuhswipp.supabase.co',
   supabaseKey:'sb_publishable_3E5BMGRcfKRt0MBFXPTfwg_lexboTMm'
