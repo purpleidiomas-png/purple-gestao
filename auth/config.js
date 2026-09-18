754b44ddb8bf4d521ec6bfee1c7ebdb839b41715
Raphael Moraes <raphaelmoraes@MacBook-Air-de-Raphael.local>
2026-09-18 19:12:11 -0300

    Pré-cadastra unidades Inspire

--- auth/config.js
+++ auth/config.js
@@ -1,6 +1,6 @@
 window.PurpleAuthConfig=Object.freeze({
-  appVersion:'1.13.41-teacher-home-link',
-  serviceWorkerVersion:'purple-gestao-v230',
+  appVersion:'1.13.43-twr-team-access',
+  serviceWorkerVersion:'purple-gestao-v232',
   lessonPlansStorageBucket:'lesson-plan-assets',
   manifestVersion:'manifest.webmanifest',
   supabaseUrl:'https://qqlymzyvvgmbyuhswipp.supabase.co',
