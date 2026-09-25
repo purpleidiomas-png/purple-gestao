59672500d3e254c26ebc31dd03f65df1fbc66123
Raphael Moraes <raphaelmoraes@MacBook-Air-de-Raphael.local>
2026-09-25 16:39:10 -0300

    codex: clarify class approval rule

--- auth/config.js
+++ auth/config.js
@@ -1,6 +1,6 @@
 window.PurpleAuthConfig=Object.freeze({
-  appVersion:'1.13.71-turmas-homework-drilldown',
-  serviceWorkerVersion:'purple-gestao-v294',
+  appVersion:'1.13.72-turmas-approval-rule-copy',
+  serviceWorkerVersion:'purple-gestao-v295',
   lessonPlansStorageBucket:'lesson-plan-assets',
   manifestVersion:'manifest.webmanifest',
   supabaseUrl:'https://qqlymzyvvgmbyuhswipp.supabase.co',
